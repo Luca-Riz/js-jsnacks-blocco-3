14dc72b4e78f2dc55cbcba124cdaaf1b1dfc7ac1
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-06-10 14:58:23 +0200

    add snack 2

--- js/script.js
+++ js/script.js
@@ -1,37 +1,52 @@
 // 1. Si scriva una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).  La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
 
-var x = parseInt(prompt('inserisci posizione più piccolo'));
-var y = parseInt(prompt('inserisci posizione più grande'));
-var arrayUser = ['cane', 'pesce', 'cavallo', 'lumaca', 'asino', 'scimmia'];
+// var x = parseInt(prompt('inserisci posizione più piccolo'));
+// var y = parseInt(prompt('inserisci posizione più grande'));
+// var arrayUser = ['cane', 'pesce', 'cavallo', 'lumaca', 'asino', 'scimmia'];
 
-var newArray = rangeArray(x,y,arrayUser);
-console.log(newArray);
+// var newArray = rangeArray(x,y,arrayUser);
+// console.log(newArray);
 
-//funzione
+// //funzione
 
-function rangeArray(a,b,array){
-    console.log(a);
-    console.log(b);
-    var c = [];
+// function rangeArray(a,b,array){
+//     console.log(a);
+//     console.log(b);
+//     var c = [];
 
-    if(a>b){ /* swap */
-        var temp = a;
-        a = b;
-        b = temp;
-    }
+//     if(a>b){ /* swap */
+//         var temp = a;
+//         a = b;
+//         b = temp;
+//     }
 
-    for(var i=a+1; i<b; i++){
-        c.push(array[i]);
-    }
-    return c;
-}
+//     for(var i=a+1; i<b; i++){
+//         c.push(array[i]);
+//     }
+//     return c;
+// }
 
 //========================================================================================
 // 2. Dare la possibilità di inserire due parole. 
 //        Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.    Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
+var x = prompt('inserisci prima parola');
+var y = prompt('inserisci seconda parola');
 
+var pLunga = parolaLunga(x,y);
+console.log(pLunga);
 
+// funzione
+
+function parolaLunga(a,b){
+    if(a.length>b.length){
+        return a;
+    } else if (a.length<b.length){
+        return b;
+    } else {
+        return a + ' ' + b;
+    }
+}
 
 //========================================================================================
 // 3.  Scrivi una funzione che fonda due array (con lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall’altroes. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
